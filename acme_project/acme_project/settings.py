2bc41cd452faf497fe674a8d2a634c001811ad0a
Zi0001 <WorckAcTop.218@gmail.com>
2024-07-17 16:24:01 +0300

    final_po_formam_nado_povtorit

--- acme_project/acme_project/settings.py
+++ acme_project/acme_project/settings.py
@@ -17,7 +17,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'birthday.apps.BirthdayConfig',
     'pages.apps.PagesConfig',
-    'django_bootstrap5'
+    'django_bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -73,6 +73,10 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+
+
+MEDIA_ROOT = BASE_DIR / 'media'
 
 LANGUAGE_CODE = 'ru-RU'
 
